673841fe471e655fd7b5049975edd1b76f75da2e
Dkreick <damian.kreick@gmail.com>
2018-05-31 11:42:35 -0300

    Adds example image

---
+++ Inventory System/Assets/ItemManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    void Start()
+    {
+        
+    }
+
+    void AddItem()
+    {
+		
+    }
+
+    void RemoveItem()
+    {
+
+    }
+}
