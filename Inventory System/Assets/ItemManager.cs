218cfda51a3cb3cb5d27cbe5969920e504b938c4
DKreick <damian.kreick@gmail.com>
2018-08-28 17:50:23 -0300

    Implements item drag

---
+++ Inventory System/Assets/ItemManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+	private bool _isDragged;
+	private Vector3 mousePosition;
+
+	public void OnDrag()
+	{
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePosition.z = 1;
+		transform.position = mousePosition;
+	}
+}
